60811c6a9e87e62f9d882c9e3fcbf62b8b88c436
dann <austindan168@gmail.com>
2024-10-13 23:30:47 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 // index.js
+fetch("http://localhost:3000/ramens")
+.then(response => response.json())
+.then(data => {
+  console.log(data)
+  data.forEach(ramen => {
+    displayRamens(ramen)
+    })
+
+  })
+  .catch(err => console.log(err))
 
 // Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+function handleClick(ramen) {
+    let ramenDetail = document.getElementById("ramen-detail")
+    ramenDetail.innerHTML = `
+    <img src=${ramen.image}>
+    <h2>${ramen.name}</h2>
+    <h3>${ramen.restaurant}</h3>
+    `
+}
 
 const addSubmitListener = () => {
   // Add code
+  let plusRamen = document.getElementById("new-ramen")
+  plusRamen.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const newRamen = new FormData(e.target)
+    const freshRamen = {
+      name:newRamen.get("name"),
+      restaurant:newRamen.get("restaurant"),
+      image:newRamen.get("image"),
+      rating:newRamen.get("rating"),
+      comment:newRamen.get("comment")
+    }
+    fetch("http://localhost:3000/ramens", {
+      method: "POST",
+      headers: {
+        'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(freshRamen)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => console.log(error))
+  })
 }
 
-const displayRamens = () => {
-  // Add code
-};
+function displayRamens(ramen) {
+  let theRamen = document.createElement("div")
+  theRamen.innerHTML = `
+  <img src=${ramen.image}>
+  <p>${ramen.name}</p>
+  `
+  let ramenMenu = document.getElementById("ramen-menu")
+  ramenMenu.appendChild(theRamen)
+  let imge = theRamen.querySelector("img")
+  imge.addEventListener("click", () => {
+    handleClick(ramen)
+  })
+}
+
 
 const main = () => {
   // Invoke displayRamens here
